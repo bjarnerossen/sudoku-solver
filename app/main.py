ab76dd39752bd8e83d47bb21ad2711bafff15355
Bjarne <92723881+bjarnerossen@users.noreply.github.com>
2023-03-15 18:07:57 +1300

    functionality in place

--- app/main.py
+++ app/main.py
@@ -1,16 +1,22 @@
-import random
 import copy
 import terminaltables
 from termcolor import colored
+import sys
+from helpers import *
 
 def main():
+    # Define the difficulty levels as a dictionary
+    DIFFICULTY_LEVELS = {
+        1: "Easy",
+        2: "Medium",
+        3: "Intermediate",
+        4: "Extreme"
+    }
+
     # Define the menu options
     options = [
         ["Difficulty", "Description"],
-        ["1", "Easy"],
-        ["2", "Medium"],
-        ["3", "Intermediate"],
-        ["4", "Extreme"]
+        *[[str(k), v] for k, v in DIFFICULTY_LEVELS.items()]
     ]   
 
     # Create the table
@@ -28,6 +34,8 @@ def main():
             break
         except ValueError:
             print("Invalid input. Please enter a number between 1 and 4.")
+        except KeyboardInterrupt:
+            sys.exit()
 
     # Generate Sudoku board and its copy
     puzzle = generate_sudoku_board(difficulty)
@@ -36,96 +44,41 @@ def main():
     # Solve Sudoku board
     solution = solve_sudoku(puzzle)
     
-    # Print the puzzle and its solution
-    print(colored("PUZZLE:", attrs=["bold"]))
+    print(colored("\nPUZZLE:", attrs=["bold"]))
     print_puzzle(puzzle_copy)
-    print()
-    print(colored("SOLUTION:", attrs=["bold"]))
-    print_solution(puzzle_copy, solution)
+    while True:
+        try:
+            user_input = str(input())
+            if not validate_user_input(user_input):
+                raise ValueError
+            if user_input.lower() in ["help", "h"]:
+                print(colored(
+"""
+Sudoku is a logic-based number placement puzzle. 
+The goal is to fill a 9x9 grid with digits so that each column, each row, and each of the nine 3x3 sub-grids contains all of the digits from 1 to 9. 
+Each digit can only appear once in each row, column, and sub-grid. 
+Good luck!
+"""
+                , attrs=["blink"]))
+            elif user_input.lower() in ["solve", "s"]:
+                    # Print the puzzle and its solution
+                    print()
+                    print(colored("SOLUTION:", attrs=["bold"]))
+                    print_solution(puzzle_copy, solution)
+                    break
+        except ValueError:
+                print('Command not found. Options: "help", "solve", "h", "s"')
+            
+        except KeyboardInterrupt:
+            sys.exit()
 
-def generate_sudoku_board(difficulty=1):
-    # create empty board only containing zeros
-    board = [[0 for _ in range(9)] for _ in range(9)]
-
-    # fill in first row randomly
-    for i in range(9):
-        board[0][i] = i + 1
-    random.shuffle(board[0])
-
-    # fill in the rest of board
-    solve_sudoku(board)
-
-    # define num_cells based on difficulty
-    if difficulty == 1:
-        num_cells = 20
-    elif difficulty == 2:
-        num_cells = 30
-    elif difficulty == 3:
-        num_cells = 40
-    elif difficulty == 4:
-        num_cells = 60
-
-    # randomly remove cells based on given difficulty
-    remove_cells(board, num_cells)
-
-    return board
-
-def solve_sudoku(board):
-    # find the first empty cell
-    row, col = find_empty_cell(board)
-
-    # if no empty cell is found, the sudoku is solved
-    if row == -1:
-        return board
-    
-    # try each possible number in the cell
-    for num in range(1, 10):
-        if is_valid_move(board, row, col, num):
-            board[row][col] = num
-
-            if solve_sudoku(board):
-                return board
-
-    # if we tried all numbers and none worked, backtrack
-    board[row][col] = 0
-
-def find_empty_cell(board):
-    # find empty cell in board and return index for row and col
-    for row in range(9):
-        for col in range(9):
-            if board[row][col] == 0:
-                return row, col
-    return -1, -1
-
-def is_valid_move(board, row, col, num):
-    # check row
-    for i in range(9):
-        if board[row][i] == num:
-            return False
-    
-    # check column
-    for i in range(9):
-        if board[i][col] == num:
-            return False
-
-    # check subgrid
-    subgrid_row = (row // 3) * 3
-    subgrid_col = (col // 3) * 3
-    for i in range(subgrid_row, subgrid_row + 3):
-        for j in range(subgrid_col, subgrid_col + 3):
-            if board[i][j] == num:
-                return False
-    
-    # if all checks pass, the move is valid
-    return True
-
-def remove_cells(board, num_cells):
-    # remove cells randomly from board based on difficulty level
-    for _ in range(num_cells):
-        row, col = random.randint(0, 8), random.randint(0, 8)
-        while board[row][col] == 0:
-            row, col = random.randint(0, 8), random.randint(0, 8)
-        board[row][col] = 0
+def validate_user_input(user_input):
+    """
+    Validates user input for "help" and "solve" commands.
+    Returns True if the input is valid, False otherwise.
+    """
+    valid_commands = ["help", "solve", "h", "s"]
+    return user_input.lower() in valid_commands
 
 def print_puzzle(board):
     for i in range(len(board)):
