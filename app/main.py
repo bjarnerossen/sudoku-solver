29a1dcf2b5ece24f10ecf1b0cc90e995e9b7c880
Bjarne <92723881+bjarnerossen@users.noreply.github.com>
2023-03-16 19:34:06 +1300

    neat board print in place

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
+# Third party
 from copy import deepcopy # Used to create a deep copy of the Sudoku board
-import terminaltables # Used to print the user menu in a tabular format
 from termcolor import colored # Used to colorize output text
-import sys # Used to handle keyboard interrupt and exit the program
+import terminaltables # Used to print the user menu in a tabular format
+import datetime
 
+# Own files
 import helpers # Contains helper functions to generate and solve Sudoku puzzles
+
+# Libraries to generate pdf
+from reportlab.pdfgen import canvas # Used to create PDF documents
+from reportlab.lib.pagesizes import letter # Used to set the page size for PDF documents
+from reportlab.lib.units import inch # Used to set the measurement unit for PDF documents
+
 
 # Define the difficulty levels as a dictionary
 DIFFICULTY_LEVELS = {
@@ -20,107 +28,189 @@ def main():
     # Get user input for difficulty level
     difficulty = get_user_difficulty()
 
-    # Generate Sudoku board and its copy
-    puzzle = helpers.generate_sudoku_board(difficulty)
-    puzzle_copy = deepcopy(puzzle)
-    
-    # Solve Sudoku board
-    solution = helpers.solve_sudoku(puzzle)
-    
-    # Print the original puzzle
-    print(colored("\nPUZZLE:", attrs=["bold"]))
-    print_puzzle(puzzle)
+    # Get user input for number of Sudokus to generate
+    num_sudokus = get_num_sudokus()
 
-    # Prompt the user for input and provide options
-    while True:
-        try:
-            user_input = str(input())
-            if not validate_user_input(user_input):
-                raise ValueError
-            if user_input.lower() in ["help", "h"]:
-                print_help_message()
-            elif user_input.lower() in ["solve", "s"]:
-                    # Print the puzzle and its solution
-                    print()
-                    print(colored("SOLUTION:", attrs=["bold"]))
-                    print_solution(puzzle_copy, solution)
-                    break
-        except ValueError:
-                print('Command not found. Options: "help", "solve", "h", "s"')
-        except KeyboardInterrupt:
-            sys.exit()
+    # Initialize the PDF document
+    c = canvas.Canvas(f"{datetime.date.today()}-{DIFFICULTY_LEVELS[difficulty]}-sudokus.pdf", pagesize=letter)
 
+    # Set the title of the PDF document
+    c.setTitle("Sudoku Puzzles")
+
+    # Print the cover page
+    c.setFont("Helvetica-Bold", 24)
+    c.drawString(inch, 10*inch, "Sudoku Puzzles")
+    c.setFont("Helvetica", 14)
+    c.drawString(inch, 9*inch, f"Difficulty Level: {DIFFICULTY_LEVELS[difficulty]}")
+    c.drawString(inch, 8*inch, f"Number of Sudokus: {num_sudokus}")
+    c.showPage()
+
+    # Generate the Sudokus and store the puzzles and solutions in lists
+    puzzles = []
+    solutions = []
+    for i in range(1, num_sudokus+1):
+        # Generate Sudoku board and its copy
+        puzzle = helpers.generate_sudoku_board(difficulty)
+        puzzle_copy = deepcopy(puzzle)
+
+        # Solve Sudoku board
+        solution = helpers.solve_sudoku(puzzle)
+
+        # Add the puzzle and solution to the lists
+        puzzles.append(puzzle)
+        solutions.append(solution)
+
+        # Print the original puzzle on the PDF document
+        if i % 2 == 1:
+            # Top half of the page
+            x = 2.25 * inch
+            y = 9.5 * inch - (i//2 * 2.25 * inch)
+        else:
+            # Bottom half of the page
+            x = 5.75 * inch
+            y = 9.5 * inch - (i//2 - 1) * 2.25 * inch
+        print_puzzle_to_pdf(c, puzzle_copy, x, y)
+
+        # Print progress
+        print(f"Generated {i}/{num_sudokus} Sudokus")
+
+        # If we've printed two puzzles, move to the next page
+        if i % 2 == 0:
+            c.showPage()
+
+    # Save the PDF document
+    c.save()
+
+    # Print confirmation message
+    print(f"\nSudokus generated and saved to 'sudokus.pdf'")
+    
 def print_user_menu():
-    """Prints the user menu."""
-    table = terminaltables.AsciiTable([
-        ["Difficulty", "Description"],
-        *[[str(k), v] for k, v in DIFFICULTY_LEVELS.items()]
-    ])
+    # Define the user menu as a list of tuples
+    menu = [
+    ("1", "Easy"),
+    ("2", "Medium"),
+    ("3", "Intermediate"),
+    ("4", "Extreme")
+    ]
+    # Define the table headers
+    headers = ["Difficulty Level", "Description"]
+    
+    # Create a table object with the menu and headers
+    table = terminaltables.AsciiTable([headers] + menu)
+    
+    # Print the table to the console
     print(table.table)
 
 def get_user_difficulty():
-    """Prompts the user to select a difficulty level and returns the selected level."""
+    # Get user input for difficulty level
     while True:
         try:
-            difficulty = int(input("Choose difficulty (1-4): "))
-            if difficulty < 1 or difficulty > 4:
-                raise ValueError
-            break
+            difficulty = int(input("Enter the difficulty level (1-4): "))
+            if difficulty in range(1,5):
+                return difficulty
+            else:
+                print("Invalid input. Please enter a number between 1 and 4.")
         except ValueError:
             print("Invalid input. Please enter a number between 1 and 4.")
-        except KeyboardInterrupt:
-            sys.exit()
-    return difficulty
 
-def validate_user_input(user_input):
-    """
-    Validates user input for "help" and "solve" commands.
-    Returns True if the input is valid, False otherwise.
-    """
-    valid_commands = ["help", "solve", "h", "s"]
-    return user_input.lower() in valid_commands
 
-def print_help_message():
-    """Prints a brief description of the rules of Sudoku."""
-    print(colored(
-"""
-Sudoku is a logic-based number placement puzzle. 
-The goal is to fill a 9x9 grid with digits so that each column, each row, and each of the nine 3x3 sub-grids contains all of the digits from 1 to 9. 
-Each digit can only appear once in each row, column, and sub-grid. 
-Good luck!
-""", attrs=["blink"]))
-
-def print_puzzle(board):
-    """Prints the Sudoku board in a grid format with empty cells represented as '?'."""
-    for i in range(len(board)):
-        if i % 3 == 0:
-            print("+-------+-------+-------+")
-        for j in range(len(board[i])):
-            if j % 3 == 0:
-                print("| ", end="")
-            if board[i][j] == 0:
-                print(colored("?", "white", "on_black", attrs=["bold"]) + " ", end="")
+def get_num_sudokus():
+    # Get user input for number of Sudokus to generate
+    while True:
+        try:
+            num_sudokus = int(input("Enter the number of Sudokus to generate: "))
+            # Check if the input is positive
+            if 0 < num_sudokus < 20:
+                return num_sudokus
             else:
-                print(str(board[i][j]) + " ", end="")
-            if j == 8:
-                print("|")
-    print("+-------+-------+-------+")
+                print("Invalid input. Please enter a positive number less than 20.")
+        except ValueError:
+            print("Invalid input. Please enter a positive number.")
+            
+def print_puzzle_to_pdf(c, puzzle, x, y):
+    # Define the font for the Sudoku puzzle
+    c.setFont("Helvetica-Bold", 18)
 
-def print_solution(puzzle, solved):
-    """Prints the solved Sudoku board in a grid format with solutions in light green."""
-    for i in range(len(solved)):
+    # Loop over the rows and columns of the puzzle and print the values
+    for row in range(9):
+        for col in range(9):
+            # Determine the color of the value (black for given values, red for empty values)
+            color = "black" if puzzle[row][col] != 0 else "red"
+    
+            # Print the value with the appropriate color at the appropriate position
+            c.setFillColor(color)
+            c.drawString(x + col * 0.4 * inch, y - row * 0.4 * inch, str(puzzle[row][col]))
+    
+
+def print_solution_to_pdf(c, puzzle, solution, x, y):
+    # Print the label for the solution
+    c.setFont("Helvetica-Bold", 14)
+    c.drawString(x, y - 0.25 * inch, "Solution:")
+
+    # Set the color of the text to green for the solved cells
+    c.setFont("Helvetica", 12)
+    for row in range(9):
+        for col in range(9):
+            if puzzle[row][col] == 0:
+                c.setFillColorRGB(0, 0.6, 0)
+                c.drawString(x + col * 0.35 * inch, y - (row + 1) * 0.35 * inch, str(solution[row][col]))
+                c.setFillColorRGB(0, 0, 0)
+
+    # Draw the horizontal and vertical lines
+    for i in range(10):
         if i % 3 == 0:
-            print("+-------+-------+-------+")
-        for j in range(len(solved[i])):
-            if j % 3 == 0:
-                print("| ", end="")
-            if puzzle[i][j] == 0:
-                print(colored(str(solved[i][j]) + " ", "green", attrs=["bold"]), end="")
-            else:
-                print(str(solved[i][j]) + " ", end="")
-            if j == 8:
-                print("|")
-    print("+-------+-------+-------+")
+            c.setLineWidth(2)
+        else:
+            c.setLineWidth(1)
+        c.line(x, y - i * 0.35 * inch, x + 2.8 * inch, y - i * 0.35 * inch)
+        c.line(x + i * 0.35 * inch, y, x + i * 0.35 * inch, y - 2.8 * inch)
+
+    # Draw the borders
+    c.setLineWidth(2)
+    c.line(x, y, x + 2.8 * inch, y)
+    c.line(x, y - 2.8 * inch, x + 2.8 * inch, y - 2.8 * inch)
+    c.line(x, y, x, y - 2.8 * inch)
+    c.line(x + 2.8 * inch, y, x + 2.8 * inch, y - 2.8 * inch)
+
+    # Move to the next position
+    if x == 3 * inch:
+        c.showPage()
+        x = 0.5 * inch
+        y -= 3 * inch
+    else:
+        x += 3 * inch
+
+# def print_puzzle(board):
+#     """Prints the Sudoku board in a grid format with empty cells represented as '?'."""
+#     for i in range(len(board)):
+#         if i % 3 == 0:
+#             print("+-------+-------+-------+")
+#         for j in range(len(board[i])):
+#             if j % 3 == 0:
+#                 print("| ", end="")
+#             if board[i][j] == 0:
+#                 print(colored("?", "white", "on_black", attrs=["bold"]) + " ", end="")
+#             else:
+#                 print(str(board[i][j]) + " ", end="")
+#             if j == 8:
+#                 print("|")
+#     print("+-------+-------+-------+")
+
+# def print_solution(puzzle, solved):
+#     """Prints the solved Sudoku board in a grid format with solutions in light green."""
+#     for i in range(len(solved)):
+#         if i % 3 == 0:
+#             print("+-------+-------+-------+")
+#         for j in range(len(solved[i])):
+#             if j % 3 == 0:
+#                 print("| ", end="")
+#             if puzzle[i][j] == 0:
+#                 print(colored(str(solved[i][j]) + " ", "green", attrs=["bold"]), end="")
+#             else:
+#                 print(str(solved[i][j]) + " ", end="")
+#             if j == 8:
+#                 print("|")
+#     print("+-------+-------+-------+")
 
 if __name__ == "__main__":
     main()
---
+++ src/main.py
@@ -0,0 +1,161 @@
+from copy import deepcopy # Used to create a deep copy of the Sudoku board
+import terminaltables # Used to print the user menu in a tabular format
+from termcolor import colored # Used to colorize output text
+import sys # Used to handle keyboard interrupt and exit the program
+
+import itertools
+import helpers # Contains helper functions to generate and solve Sudoku puzzles
+
+# Define the difficulty levels as a dictionary
+DIFFICULTY_LEVELS = {
+    1: "Easy",
+    2: "Medium",
+    3: "Intermediate",
+    4: "Extreme"
+}
+
+def main():
+    # Print the user menu
+    print_user_menu()
+
+    # Get user input for difficulty level
+    difficulty = get_user_difficulty()
+
+    # Generate Sudoku board and its copy
+    puzzle = helpers.generate_sudoku_board(difficulty)
+    puzzle_copy = deepcopy(puzzle)
+    
+    # Solve Sudoku board
+    solution = helpers.solve_sudoku(puzzle)
+    
+    # Print the original puzzle
+    print(colored("\nPUZZLE:", attrs=["bold"]))
+    print_puzzle(puzzle_copy)
+
+    # Prompt the user for input and provide options
+    while True:
+        try:
+            user_input = str(input())
+            if not validate_user_input(user_input):
+                raise ValueError
+            if user_input.lower() in ["help", "h"]:
+                print_help_message()
+            elif user_input.lower() in ["solve", "s"]:
+                    # Print the puzzle and its solution
+                    print()
+                    print(colored("SOLUTION:", attrs=["bold"]))
+                    print_solution(puzzle_copy, solution)
+                    break
+        except ValueError:
+                print('Command not found. Options: "help", "solve", "h", "s"')
+        except KeyboardInterrupt:
+            sys.exit()
+
+def print_user_menu():
+    """Prints the user menu."""
+    table = terminaltables.AsciiTable([
+        ["Difficulty", "Description"],
+        *[[str(k), v] for k, v in DIFFICULTY_LEVELS.items()]
+    ])
+    print(table.table)
+
+def get_user_difficulty():
+    """Prompts the user to select a difficulty level and returns the selected level."""
+    while True:
+        try:
+            difficulty = int(input("Choose difficulty (1-4): "))
+            if difficulty < 1 or difficulty > 4:
+                raise ValueError
+            break
+        except ValueError:
+            print("Invalid input. Please enter a number between 1 and 4.")
+        except KeyboardInterrupt:
+            sys.exit()
+    return difficulty
+
+def validate_user_input(user_input):
+    """
+    Validates user input for "help" and "solve" commands.
+    Returns True if the input is valid, False otherwise.
+    """
+    valid_commands = ["help", "solve", "h", "s"]
+    return user_input.lower() in valid_commands
+
+def print_help_message():
+    """Prints a brief description of the rules of Sudoku."""
+    print(colored(
+"""
+Sudoku is a logic-based number placement puzzle. 
+The goal is to fill a 9x9 grid with digits so that each column, each row, and each of the nine 3x3 sub-grids contains all of the digits from 1 to 9. 
+Each digit can only appear once in each row, column, and sub-grid. 
+Good luck!
+""", attrs=["blink"]))
+
+def print_puzzle(board):
+    side    = len(board)
+    base    = int(side**0.5)
+    def expandLine(line):
+        return line[0]+line[5:9].join([line[1:5]*(base-1)]*base)+line[9:]
+    line0  = "  "+expandLine("╔═══╤═══╦═══╗")
+    line1  = "# "+expandLine("║ . │ . ║ . ║ #")
+    line2  = "  "+expandLine("╟───┼───╫───╢")
+    line3  = "  "+expandLine("╠═══╪═══╬═══╣")
+    line4  = "  "+expandLine("╚═══╧═══╩═══╝")
+
+    symbol = " 123456789" if base <= 3 else " ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    nums   = [ [""]+[f"({symbol[-n]})" if n<0 else f" {symbol[n]} "  for n in row]
+               for row in board ]
+    coord  = "   "+"".join(f" {s}  " for s in symbol[1:side+1])
+    lines  = []
+    lines.append(coord)
+    lines.append(line0)
+    for r in range(1,side+1):
+        line1n = line1.replace("#",str(symbol[r]))
+        lines.append( "".join(n+s for n,s in zip(nums[r-1],line1n.split(" . "))) )
+        lines.append([line2,line3,line4][(r%side==0)+(r%base==0)])
+    lines.append(coord)
+    print(*lines,sep="\n")
+
+def print_solution(puzzle, solved):
+    side    = len(puzzle)
+    base    = int(side**0.5)
+    def expandLine(line):
+        return line[0]+line[5:9].join([line[1:5]*(base-1)]*base)+line[9:]
+    line0  = "  "+expandLine("╔═══╤═══╦═══╗")
+    line1  = "# "+expandLine("║ . │ . ║ . ║ #")
+    line2  = "  "+expandLine("╟───┼───╫───╢")
+    line3  = "  "+expandLine("╠═══╪═══╬═══╣")
+    line4  = "  "+expandLine("╚═══╧═══╩═══╝")
+
+    symbol = " 123456789" if base <= 3 else " ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    nums   = [ [""]+[f"({symbol[-n]})" if n<0 else f" {symbol[n]} "  for n in row]
+               for row in puzzle ]
+    coord  = "   "+"".join(f" {s}  " for s in symbol[1:side+1])
+    lines  = []
+    lines.append(coord)
+    lines.append(line0)
+    for r in range(1,side+1):
+        line1n = line1.replace("#",str(symbol[r]))
+        lines.append( "".join(n+s for n,s in zip(nums[r-1],line1n.split(" . "))) )
+        lines.append([line2,line3,line4][(r%side==0)+(r%base==0)])
+    lines.append(coord)
+    print(*lines,sep="\n")
+
+    # #________________________________________________
+    # """Prints the solved Sudoku board in a grid format with solutions in light green."""
+    # for i in range(len(solved)):
+    #     if i % 3 == 0:
+    #         print("+-------+-------+-------+")
+    #     for j in range(len(solved[i])):
+    #         if j % 3 == 0:
+    #             print("| ", end="")
+    #         if puzzle[i][j] == 0:
+    #             print(colored(str(solved[i][j]) + " ", "green", attrs=["bold"]), end="")
+    #         else:
+    #             print(str(solved[i][j]) + " ", end="")
+    #         if j == 8:
+    #             print("|")
+    # print("+-------+-------+-------+")
+
+if __name__ == "__main__":
+    main()
